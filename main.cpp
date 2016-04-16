ad2dfa29fa7c747b4d69f64c7793dd83fb319a88
Taciturn-knight <1005324930@qq.com>
2016-04-16 09:41:17 +0800

    1

--- main.cpp
+++ main.cpp
@@ -10,6 +10,6 @@ int main()
     o_file_date a;
     sp3_file b;
     file_read test;
-    //test.ppp_o_read("C://PPP//readFile//oFile//cas11680.15o",a);
+    test.ppp_o_read("C://PPP//readFile//oFile//cas11680.15o",a);
     test.ppp_sp3_read("C:/PPP/readFile/sp3/gbm18492.sp3",b);
 }
